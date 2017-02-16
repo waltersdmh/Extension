e65344c007639b47471d4867e1b33b274e651a50
Daniel Walters <daniel-walters@outlook.com>
2017-02-16 21:14:07 +0000

    Appearence Changes

--- popup.js
+++ popup.js
@@ -1,12 +1,11 @@
-
-function getCurrentTabUrl(callback) {  
+function getCurrentTabUrl(callback) {
   var queryInfo = {
-    active: true, 
+    active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0]; 
+    var tab = tabs[0];
     var url = tab.url;
     callback(url);
   });
@@ -14,131 +13,114 @@ function getCurrentTabUrl(callback) {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  getCurrentTabUrl(function(url) {
+    console.log(url);
+
+    // log function
+    log = function(data) {
+      $("div#terminal").prepend("</br>" + data);
+      console.log(data);
+    };
 
 
+    $(document).ready(function() {
+      $("div#message_details").hide()
 
 
+      var ws;
+      var host = "192.168.0.19";
+      var port = "8888"
+      var uri = "/ws"
+
+      // create websocket instance
+      ws = new WebSocket("ws://" + host + ":" + port + uri);
+
+      // Handle incoming websocket message callback
+      ws.onmessage = function(evt) {
+        evt.preventDefault();
+
+        //       log("Message Received: " + evt.data)
+        var sep = "\",";
+        reviews = evt.data.split(sep)
+        clickHandler();
+        for (i = 0; i < reviews.length; i++) {
+          //        var para = document.createElement("tr");
+          //       var t = document.createTextNode(reviews[i]);     
+          //        para.appendChild(t);     
+
+          var table = document.getElementById("table");
+          var row = table.insertRow(i);
+          var cell = row.insertCell(0);
+          cell.innerHTML = reviews[i];
+          //      $("table").highlight(["watch", "good", "bad"]);
+
+          //      console.log(strings);
 
 
-document.addEventListener('DOMContentLoaded', function () {
-
-document.getElementById('deletestuff').addEventListener('click', clickHandler);
-
-getCurrentTabUrl(function(url) {
-    
-    
-  
-  console.log(url);
-  
-      // log function
-      log = function(data){
-        $("div#terminal").prepend("</br>" +data);
-        console.log(data);
-      };
- 
-      $(document).ready(function () {
-        $("div#message_details").hide()
- 
-        var ws;
- 
-        $("#open").click(function(evt) {
-          evt.preventDefault();
- 
-          var host = "localhost";
-          var port = "8888"
-          var uri = "/ws"
- 
-          // create websocket instance
-          ws = new WebSocket("ws://" + host + ":" + port + uri);
-           
-          // Handle incoming websocket message callback
-          ws.onmessage = function(evt) {
-            
-     //       log("Message Received: " + evt.data)
-            var sep = "\",";
-            reviews = evt.data.split(sep)
-            clickHandler();
-            for (i = 0; i < reviews.length; i++){
-      //        var para = document.createElement("tr");
-       //       var t = document.createTextNode(reviews[i]);     
-      //        para.appendChild(t);     
-
-var table = document.getElementById("table");
-           var row = table.insertRow(i);
-           var cell = row.insertCell(0);
-           cell.innerHTML = reviews[i];
-     //      $("table").highlight(["watch", "good", "bad"]);
-
-    //      console.log(strings);
-
-         
-                                          // Append the text to <p>
-           //   document.getElementById("test").appendChild(para);
+          // Append the text to <p>
+          //   document.getElementById("test").appendChild(para);
           //    var span = $("test");
-         //     span.html(span.html().replace(/#camera/g, '<span style="color: red">$&</span>'));
-              
-            }
+          //     span.html(span.html().replace(/#camera/g, '<span style="color: red">$&</span>'));
 
-            var strings = $("#message").val();
-            words = strings.toString();
-            var array = words.split(" ");
+        }
 
-
-             for (i = 0; i < array.length; i++){
-           $("table").highlight(array[i]);  
-          }
+        var strings = $("#message").val();
+        words = strings.toString();
+        var array = words.split(" ");
 
 
-          $("table").highlight("good", { className: 'good' });
-          $("table").highlight("bad", { className: 'bad' });
-                                   // Create a <p> element
-       // Create a text node
-
-          
+        for (i = 0; i < array.length; i++) {
+          $("table").highlight(array[i]);
+        }
 
 
-
-            };
- 
-          // Close Websocket callback
-          ws.onclose = function(evt) {
-            log("***Connection Closed***");
-            alert("Connection close");
-            $("#host").css("background", "#ff0000"); 
-            $("#port").css("background", "#ff0000"); 
-            $("#uri").css("background",  "#ff0000");
-            $("div#message_details").empty();
- 
-            };
- 
-          // Open Websocket callback
-          ws.onopen = function(evt) { 
-            $("#host").css("background", "#00ff00"); 
-            $("#port").css("background", "#00ff00"); 
-            $("#uri").css("background", "#00ff00");
-            $("div#message_details").show();
-            log("***Connection Opened***");
-          };
+        $("table").highlight(["good", "great", "nice", "thankful", "amazed", "amazing", "fantastic", "glad", "happy", "helpful", "pleased"], {
+          className: 'good'
         });
- 
-        // Send websocket message function
-        $("#send").click(function(evt) {
-            log("Sending Message: "+$("#message").val());
-            ws.send($("#message").val()+","+url);
-            
+        $("table").highlight(["bad", "wrong", "awful", "delightful", "dislike", "annoyed", "hopeless", "sad"], {
+          className: 'bad'
         });
- 
-      });
-});
+        // Create a <p> element
+        // Create a text node
+      };
+
+      // Close Websocket callback
+      ws.onclose = function(evt) {
+     //   log("***Connection Closed***");
+        /*alert("Connection close");
+        $("#host").css("background", "#ff0000");
+        $("#port").css("background", "#ff0000");
+        $("#uri").css("background", "#ff0000");*/
+        $("div#message_details").empty();
+
+      };
+
+      // Open Websocket callback
+      ws.onopen = function(evt) {
+        //$("#host").css("background", "#00ff00");
+        //  $("#port").css("background", "#00ff00");
+        //  $("#uri").css("background", "#00ff00");
+        $("div#message_details").show();
+   //     log("***Connection Opened***");
+      };
 
 
+      // Send websocket message function
+      $("#send").click(function(evt) {
+      //  log("Sending Message: " + $("#message").val());
+        ws.send($("#message").val() + "," + url);
+      })
+    });
+
+  });
 });
 
 function clickHandler() {
   table = document.getElementById("table");
-  while(table.rows.length > 0) {
-  table.deleteRow(0);
-}
+  while (table.rows.length > 0) {
+    table.deleteRow(0);
+  }
 }
 
 
