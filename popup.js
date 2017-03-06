1af905a0e453fab43b7f8dc4084200aa0fdf274f
Daniel Walters <daniel-walters@outlook.com>
2017-03-06 01:06:59 +0000

    Host

--- popup.js
+++ popup.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
       var ws;
-      var host = "localhost";
+      var host = "192.168.0.19";
       var port = "8888"
       var uri = "/ws"
 
