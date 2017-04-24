e55af26628aece57d63556120d9c30148e10590b
Daniel Walters <daniel-walters@outlook.com>
2017-04-24 23:17:57 +0100

    Comments

--- README.md
+++
@@ -1,8 +0,0 @@
-# Extension
-Reviews Filter Extension
-
-
-To do:
-- options for highlightning
-- Firefox compatibility
-
--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+//Get the current tab's URL - it includes the product ASIN
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
@@ -11,8 +12,7 @@ function getCurrentTabUrl(callback) {
   });
 }
 
-
-
+//on document load
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // log function for debugging
@@ -23,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
     $(document).ready(function() {
       $("div#message_details").hide()
 
+      //host address and port via websocket connection
       var ws;
       var host = "82.20.252.21"; //host IP
       var port = "8888" //host receiving port
@@ -34,32 +35,33 @@ document.addEventListener('DOMContentLoaded', function() {
       // on incoming message 
       ws.onmessage = function(evt) {
         evt.preventDefault();
-        hidden = document.getElementById("hidden");
+        hidden = document.getElementById("hidden"); //remove loading bar
         hidden.style.display = "none";
-        var sep = "\",";
-        reviews = evt.data.split(sep);
-        clickHandler();
-        for (i = 0; i < reviews.length; i++) {
+        var sep = "\","; 
+        reviews = evt.data.split(sep); //seperate incoming results on comma
+        clickHandler(); //clear popup.html table
+        for (i = 0; i < reviews.length; i++) { //populate popup.html table
           var table = document.getElementById("table");
           var row = table.insertRow(i);
           var cell = row.insertCell(0);
-          cell.innerHTML = reviews[i].replace(/["[]/g, '');;
+          cell.innerHTML = reviews[i].replace(/["[]/g, ''); //replace unrequired symbols that were in the JSON file
 }
 
-        var strings = $("#message").val();
+        var strings = $("#message").val(); //key-terms
         words = strings.toString();
         var array = words.split(" ");
 
+        //highlighter - if selected
         var highlighter = document.getElementById("isHighlighted")
         if (highlighter.checked) {
         for (i = 0; i < array.length; i++) {
           $("table").highlight(array[i]);
         }
-
-
+        //"good" terms
         $("table").highlight(["good", "great", "nice", "thankful", "useful", "amazed", "amazing", "fantastic", "glad", "happy", "helpful", "pleased"], {
           className: 'good'
         });
+        //"bad" terms
         $("table").highlight(["bad", "wrong", "awful", "terrible", "delightful", "dislike", "annoyed", "hopeless", "sad"], {
           className: 'bad'
         });
@@ -72,11 +74,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // on open
       ws.onopen = function(evt) {
-        $("div#message_details").show();
+        $("div#message_details").show(); //show the controls
       };
 
 
       // on send clicked
+      //get filter settings + key-terms
       $("#send").click(function(evt) {
       var ratings = document.getElementById("star")
       var rating = ratings.options[ratings.selectedIndex].value;
@@ -85,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var revlen = document.getElementById("revLength")
       var lenSet = "0";
       if (revlen.checked) lenSet = "1"
-      ws.send($("#message").val() + "," + url + "," + rating + "," + chosenType + "," + lenSet);
+      ws.send($("#message").val() + "," + url + "," + rating + "," + chosenType + "," + lenSet); //concat message
       //set loading container active
        hidden = document.getElementById("hidden");
        hidden.style.display = "block";
@@ -93,7 +96,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
-
+//table row clearer
 function clickHandler() {
   table = document.getElementById("table");
   while (table.rows.length > 0) {
